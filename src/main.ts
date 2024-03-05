cd27165c340985dc0ec2e814c167fb3e9e0c1cc6
khashaaaa <forwardent01@gmail.com>
2024-03-05 21:56:19 +0800

    Improve

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,21 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
+import { json, urlencoded } from 'express'
 
 async function bootstrap() {
+
+  const port = process.env.PROG_PORT || 8080
+
   const app = await NestFactory.create(AppModule)
+
   app.enableCors({
     origin: '*',
     methods: 'GET, HEAD, PUT, PATCH, POST, DELETE, OPTIONS'
   })
-  const port = process.env.PROG_PORT || 8080
+
+  app.use(json({ limit: '50mb' }))
+  app.use(urlencoded({ extended: true, limit: '50mb' }))
+
   await app.listen(port)
 }
 
